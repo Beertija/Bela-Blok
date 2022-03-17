4f283c50e4a57e70fe17018a1dabad3beea53ee0
Tomislav Ogrinec <tomislav.ogrinec@gmail.com>
2022-03-17 15:21:27 +0100

    Added point calculator class for basic calculation operations

---
+++ lib/helpers/points_calculator.dart
@@ -0,0 +1,12 @@
+import 'dart:core';
+
+class PointsCalculator {
+  static int pointsFirstTeam = 0;
+  static int contractFirstTeam = 0;
+  static int pointsSecondTeam = 0;
+  static int contractSecondTeam = 0;
+
+  static void insert(String original, String addition) {
+    original += addition;
+  }
+}
